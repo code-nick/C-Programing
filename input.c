c73ca025470d8712013da8ea702f9910cfa75b0b
code-nick <code.dev.us@gmail.com>
2022-01-05 18:26:06 +0530

    Enter number to print in word

--- input.c
+++ input.c
@@ -14,17 +14,17 @@ char name[10] ,father[10], class[5]; // variable charecter type
      scanf("%s",class);
      printf("My Age:");
      scanf("%d",&age);
-     printf("----------Student ID----------\n");
-     printf("!\tName:%s \n ",name);
-     printf("*-----------------------------*\n");
-     printf("!\tFather:%s\n",father);
-     printf("*------------------------------*\n");
-     printf("!\tSection:%d\n",section);
-     printf("*------------------------------*\n");
-     printf("!\tclass: %s\n",class);
-     printf("*------------------------------*\n");
-     printf("!\tage: %d \n",age);
-     printf("*------------------------------*\n");
+     printf("--------------Student ID----------\n");
+     printf("\tName:%s \n ",name);
+     printf("!*------------------------------*!\n");
+     printf("\tFather:%s\n",father);
+     printf("!*------------------------------*!\n");
+     printf("\tSection:%d\n",section);
+     printf("!*------------------------------*!\n");
+     printf("\tclass: %s\n",class);
+     printf("!*------------------------------*!\n");
+     printf("\tage: %d \n",age);
+     printf("!*------------------------------*!\n");
      return 0 ; 
 
 
