6e419b5de092a91fd8b4bc3373027a6acc764f6b
code-nick <code.dev.us@gmail.com>
2022-01-05 19:38:36 +0530

    C program to Display Day of week

--- First.c
+++ First.c
@@ -1,12 +1,41 @@
 #include<stdio.h>
 int main(int argc, char const *argv[])
 {
-    int a =10;
-    if(a>15)
-    printf("enter the number:");
-    else{
-        printf("enter the worng number");
+    int a;
+    printf("Enter your day :");
+    scanf("%d",&a);
+    if(a==1)
+    {
+        printf("Son \n");
     }
-    
+     if (a==2)
+    {
+        printf("Mon\n");
+    }
+    else if (a==3)
+    {
+        printf("Tue\n");
+    }
+    else if(a==4)
+    {
+        printf("Wed\n");
+
+    }
+    else if(a==5)
+    {
+        printf("Thu\n");
+
+    }
+    else if (a==6)
+    {
+        printf("Fri\n");
+    }
+    else if (a==7)
+    {
+        printf("Sat\n");
+    }
+    else{
+        printf("Enter your worng choice\n");
+    }
 }
 
